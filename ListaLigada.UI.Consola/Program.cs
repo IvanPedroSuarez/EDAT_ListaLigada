3f96ff8c2c584af0f079c6d3a6fb1ad953fa4fa1
IvanPedroSuarez <23030344@itesa.edu.mx>
2024-11-13 07:29:45 -0600

    Version_03: Se agrego el menu.

--- ListaLigada.UI.Consola/Program.cs
+++ ListaLigada.UI.Consola/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ListaLigada.Logica;
+using System.ComponentModel.Design;
 
 internal class Program
 {
@@ -6,16 +7,91 @@ internal class Program
     {
         Console.WriteLine("Lista ligada simple \n");
 
+
         var singleList = new SingleList<string>();
+        int opcion = 0;
+        do
+        {
+            switch (opcion)
+            {
 
-        singleList.Add("Juan");
-        singleList.Add("Jose");
-        singleList.Add("Jan");
+                case 1: AgregarElemento(); break;
+                case 2: MostrarLista(); break;
+                case 3: EliminarElemento(); break;
+                //default: Console.WriteLine("La opcion no existe"); break;
 
-        Console.WriteLine(singleList);
 
+
+            }
+
+            opcion = Menu();
+
+        } while (opcion != 0);
+        void AgregarElemento()
+        {
+            Console.Write("** Agregar elementos a la lista ** \n");
+            Console.Write("Escriba el nombre del nodo: ");
+            var nombre = Console.ReadLine();
+            singleList.Add(nombre);
+
+        }
+       void MostrarLista()
+        {
+            if (singleList.isEmpty)
+            {
+                Console.WriteLine("La lista esta vacia");
+            }
+            else
+            {
+                Console.WriteLine(singleList);
+            }
+
+        }
+
+      void EliminarElemento()
+        {
+            throw new NotImplementedException();
+        }
+
+        Console.ReadKey();
+
+
+    }//fin de main
+
+  
+
+    private static int Menu()
+    {
+        Console.WriteLine("Menu \n");
+        Console.WriteLine("1. Agregar elemento");
+        Console.WriteLine("2. Mostrar Lista");
+        Console.WriteLine("3. Eliminar elemento");
+        Console.WriteLine("0. Salir");
+
+        bool valido = false;
+        int opcion = 0;
+
+        do
+        {
+            Console.WriteLine(" \n Seleccione una opcion del menu : ");
+            var opcionCapturada = Console.ReadLine();
+            Console.WriteLine("");
+            if (!int.TryParse(opcionCapturada, out opcion) || opcion < 0 || opcion > 2)
+            {
+
+                Console.WriteLine(" Elige una opcion valida \n");
+                valido = false;
+            }
+            else
+            {
+                valido = true;
+            }
+;
+
+        } while (valido == false);
+
+        return opcion;
 
 
     }
-
 }
